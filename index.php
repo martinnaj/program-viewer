fb0264c24cd0be622c32b38ca76264384f4d2ea6
martinnaj <60464290+martinnaj@users.noreply.github.com>
2020-04-11 09:18:56 +0100

    Update index.php

--- index.php
+++ index.php
@@ -19,13 +19,22 @@ function secondsToTime($seconds) {
                 </tr>
                 <?php
                 $files = glob("*.txt");
+                $names = array();
+                $times = array();
+                
                 foreach ($files as $file) {
+                    array_push($names, base64_decode(substr($file, 0, -4)));
+                    array_push($times, time() - intval(file_get_contents("./$file")));
+                }
+                
+                array_multisort($times, $names);
+                
+                for ($x = (count($names) - 1); $x >= 0; $x--) {
                     echo "<tr><td>";
-                    $name = base64_decode(substr($file, 0, -4));
-                    echo $name;
+                    echo $names[$x];
                     echo "</td>";
                     echo "<td>";
-                    echo secondsToTime(time() - intval(file_get_contents("./$file")));
+                    echo secondsToTime($times[$x]);
                     echo "</td></tr>";
                 }
                 ?>
