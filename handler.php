eb16955be83f70c95467279427dfb5c675a70ee0
martinnaj <martin@najemi.cz>
2021-06-22 10:57:08 +0100

    Cleanup for loops

--- handler.php
+++ handler.php
@@ -4,25 +4,20 @@ function secondsToTime($seconds) {
     $dtT = new \DateTime("@$seconds");
     return $dtF->diff($dtT)->format('%a days, %h hours, %i minutes and %s seconds');
 }
-$files = glob("*.txt");
-echo "<tbody><tr style=\"background-color: black\"><th>Program</th><th>Time</th></tr>";
-$names = array();
-$times = array();
 
-foreach ($files as $file) {
-    array_push($names, base64_decode(substr($file, 0, -4)));
-    array_push($times, time() - intval(file_get_contents("./$file")));
+echo '<tbody><tr style="background-color: black"><th>Program</th><th>Time</th></tr>';
+
+$names = [];
+$times = [];
+
+foreach (glob("*.txt") as $file) {
+    $names[] = base64_decode(substr($file, 0, -4));
+    $times[] = time() - intval(file_get_contents("./$file"));
 }
 
 array_multisort($times, $names);
 
-for ($x = (count($names) - 1); $x >= 0; $x--) {
-    echo "<tr><td>";
-    echo $names[$x];
-    echo "</td>";
-    echo "<td>";
-    echo secondsToTime($times[$x]);
-    echo "</td></tr>";
-}
-echo "</tbody>";
+for ($x = (count($names) - 1); $x >= 0; $x--) echo '<tr><td>'.$names[$x].'</td><td>'.secondsToTime($times[$x]).'</td></tr>';
+
+echo '</tbody>';
 ?>
